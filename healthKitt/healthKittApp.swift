0cb0217e2c3d6716dd0d3bb581a36a11b3ce38ce
bbbriann <testtiostest@gmail.com>
2024-06-08 18:06:40 +0900

    Initial Commit

---
+++ healthKitt/healthKittApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthKittApp.swift
+//  healthKitt
+//
+//  Created by brian on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct healthKittApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
